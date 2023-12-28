6bf074aef84c3da6f56f6ffe2fd93ff880bab3e7
Ritvik-lab-coder <ritviksharma822@gmail.com>
2023-12-28 21:17:26 +0530

    Added files

---
+++ Mini Calendar/script.js
@@ -0,0 +1,11 @@
+const date = document.querySelector('#date');
+const day = document.querySelector('#day');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const today = new Date();
+date.innerHTML = (today.getDate() < 10) ? '0' : '' + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
